7cbbe6e5d12f76f55884603f0911a8ed8efc5513
alhajiry <latiforce@gmail.com>
2019-11-08 15:13:01 +0700

    add alert

--- script/script.js
+++ script/script.js
@@ -1,28 +1,31 @@
 // const match = document.getElementById('match')
 
 function generate() {
+    let namaPria = document.getElementById("pria");
+    let namaWanita = document.getElementById("wanita");
 
     let randomNumber = Math.ceil(Math.random() * 100);
     // console.log(`${randomNumber}%`)
-    if (randomNumber >= 70) {
+    if (namaPria.value && namaWanita.value) {
+        if (randomNumber >= 70) {
+            localStorage.setItem("matchResult", randomNumber);
 
-        localStorage.setItem("matchResult", randomNumber);
+            return (window.location.href = "match-sucess.html");
+        } else {
+            localStorage.setItem("matchResult", randomNumber);
 
-		return (window.location.href = "match-sucess.html");
-		
+            return (window.location.href = "match-fail.html");
+        }
+        // return randomNumber
     } else {
-        
-        localStorage.setItem("matchResult", randomNumber);
-
-        return (window.location.href = "match-fail.html");
+        alert("Harap masukkan nama calon jodoh kamu");
     }
-    // return randomNumber
 }
 // console.log(generate());
 
 document.addEventListener("DOMContentLoaded", event => {
-	console.log(true);
-	/// ini buat ngecek apakah value yg kita input tadi keisi atau enggak
+    console.log(true);
+    /// ini buat ngecek apakah value yg kita input tadi keisi atau enggak
     if (localStorage.getItem("matchResult") !== null) {
         let matchResult = localStorage.getItem("matchResult");
         let container = document.getElementById("success-rate");
